7c8939abd427aa4e10e832f308623afd61b0e136
BHAGWAN0 1 <michael.ross444@gmail.com>
2019-11-19 19:00:40 -0500

    recyclerview working with static data

--- app/src/main/java/com/e/osu_id_app/Model.kt
+++ app/src/main/java/com/e/osu_id_app/Model.kt
@@ -4,14 +4,27 @@ data class SessionCard(var title: String)
 
 object Supplier {
 
+
     val sessions = listOf<SessionCard>(
        SessionCard("Morning Main Campus"),
         SessionCard("Afternoon Main Campus"),
         SessionCard("Morning Annex Campus"),
         SessionCard("Afternoon Annex Campus"),
-        SessionCard("Session1"),
-        SessionCard("Session2"),
-        SessionCard("Session3")
+        SessionCard("Session1 Session1"),
+        SessionCard("Session2 Session2"),
+        SessionCard("Session3 Session3"),
+        SessionCard("Session4"),
+        SessionCard("Session5"),
+        SessionCard("Session6"),
+        SessionCard("Session7"),
+        SessionCard("Session8"),
+        SessionCard("Session9")
     )
+
+    /*
+    val sessions = listOf<SessionCard>()
+
+    // Get directories from storage
+    */
 
 }
