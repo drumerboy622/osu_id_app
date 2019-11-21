469df574c65abb08cbaf61bbb48df49dfde2fd7d
BHAGWAN0 1 <michael.ross444@gmail.com>
2019-11-21 08:11:05 -0500

    Session card cleanup

--- app/src/main/java/com/e/osu_id_app/Model.kt
+++ app/src/main/java/com/e/osu_id_app/Model.kt
@@ -2,4 +2,6 @@ package com.e.osu_id_app
 
 import java.util.*
 
-data class SessionCard(var title: String, var dateStr: String, var progressStr: String)
+data class SessionCard(var title: String, var lstModDt: Long, var dateStr: String, var progressStr: String)
+
+fun selector(p: SessionCard): Long = p.lstModDt
