7f893d7e84829376b36b49bf297c8271d605cf80
BHAGWAN0 1 <michael.ross444@gmail.com>
2019-11-28 08:57:13 -0500

    sessionCardAdapter cleanup

--- app/src/main/java/com/e/osu_id_app/SessionCardAdapter.kt
+++ app/src/main/java/com/e/osu_id_app/SessionCardAdapter.kt
@@ -17,21 +17,24 @@ import java.io.IOException
 import java.nio.file.Files.move
 import java.nio.file.Paths
 
-
-
 class SessionCardAdapter(val context: Context, val sessioncards: List<SessionCard>) : RecyclerView.Adapter<SessionCardAdapter.MyViewHolder>(){
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
 
+        // Create the viewHolder for the sessionCard
         val view = LayoutInflater.from(context).inflate(R.layout.row, parent, false)
         return MyViewHolder(view)
     }
 
     override fun getItemCount(): Int {
+
+        // Get the number of sessionCards
         return sessioncards.size
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        // Bind the data from list to viewHolder
         val sessioncard = sessioncards[position]
         holder.setData(sessioncard, position)
     }
@@ -46,7 +49,6 @@ class SessionCardAdapter(val context: Context, val sessioncards: List<SessionCar
             {
                 itemView.imageButton5.setImageResource(R.drawable.upload1)
             }
-
 
             itemView.SessionTitle.text = sessionCard!!.title
             itemView.Session_Date.text = sessionCard!!.dateStr
@@ -80,10 +82,6 @@ class SessionCardAdapter(val context: Context, val sessioncards: List<SessionCar
                 itemView.photoPreview3.setVisibility(View.GONE)
             }
 
-
-
-
-
             // Continue Session
             itemView.imageButton4.setOnClickListener {
                 val randomIntent = Intent(context, barcode_scan::class.java)
@@ -111,35 +109,41 @@ class SessionCardAdapter(val context: Context, val sessioncards: List<SessionCar
                 dialog.show()
             }
 
-            //Turn Live upload on or off
-
+            // Cloud / Batch Upload Button
             itemView.imageButton5.setOnClickListener {
+
                 val intent = Intent(context, MainActivity::class.java)
 
-
                 if (path == "sent"){
+
+                    // For sent folder, move the files back to unsent
                     var filePath = Paths.get(file.getAbsolutePath())
                     var dir = File("/storage/emulated/0/Android/media/com.osu_id_app/unsent/" + sessionCard!!.title)
                     var newFilePath = Paths.get(dir.getAbsolutePath())
                     move(filePath, newFilePath)
+
+                    // Disable the button
+                    itemView.imageButton5.isActivated = false
+
+                    // Update the progress text
+                    itemView.Session_Number_Uploaded.text = "Moved files to Unsent folder"
                 }
 
-                else {   // Files are in unsent Folder
+                else {   
 
-
+                    // For unsent folder, initiate the batch upload, files will move upon success
                     var dir = File("/storage/emulated/0/Android/media/com.osu_id_app/unsent/" + sessionCard!!.title)
-
-                    // Initiate Batch Upload on unsent folder
                     var b: B=B(dir)
                     b.start()
 
+                    // Disable the button
+                    itemView.imageButton5.isActivated = false
+
+                    // Update the progress text
+                    itemView.Session_Number_Uploaded.text = "Live Upload running!"
                 }
 
-
-
-                context.startActivity(intent)
             }
-
 
             // Delete Session
             itemView.imageButton6.setOnClickListener {
@@ -168,34 +172,45 @@ class SessionCardAdapter(val context: Context, val sessioncards: List<SessionCar
                         deleteRecursive(stageFile)
                         context.startActivity(intent)
                     }}
-                    // Display a neutral button on alert dialog
-                    builder.setNeutralButton("Cancel") { _, _ -> }
 
-                    // Finally, make the alert dialog using builder
-                    val dialog: AlertDialog = builder.create()
+                // Display a neutral button on alert dialog
+                builder.setNeutralButton("Cancel") { _, _ -> }
 
-                    // Display the alert dialog on app interface
-                    dialog.show()
+                // Finally, make the alert dialog using builder
+                val dialog: AlertDialog = builder.create()
 
-                }
+                // Display the alert dialog on app interface
+                dialog.show()
             }
-
         }
-
+    }
 }
 
-//threading upload to background
+// Batch Upload Function threaded to background
 class B(val dir: File) : Thread()
 {
+    // Handle Batch Upload Button Click
     var filesForUpload: Array<File>? = null
     val remoteDirectoryForUploads = "OSU_ID_APP/"
 
+    // STODO: Get these Environment Variables from App Settings
+    private val ENVIRONMENT_VARIABLE_HOST = "KSFTP_HOST"
+    private val ENVIRONMENT_VARIABLE_PORT = "KSFTP_PORT"
+    private val ENVIRONMENT_VARIABLE_USERNAME = "KSFTP_USERNAME"
+    private val ENVIRONMENT_VARIABLE_PASSWORD = "KSFTP_PASSWORD"
 
+    // Initialize SFTP Client
+    private var sftpClient: SftpClient? = null
+    private fun createConnectionParameters(): SftpConnectionParameters {
+        return SftpConnectionParametersBuilder.newInstance().createConnectionParameters()
+            .withHostFromEnvironmentVariable(ENVIRONMENT_VARIABLE_HOST)
+            .withPortFromEnvironmentVariable(ENVIRONMENT_VARIABLE_PORT)
+            .withUsernameFromEnvironmentVariable(ENVIRONMENT_VARIABLE_USERNAME)
+            .withPasswordFromEnvironmentVariable(ENVIRONMENT_VARIABLE_PASSWORD)
+            .create()
+    }
 
     override fun run() {
-
-        println("44444444444444444444444444")
-        println("dir is" + dir.absolutePath)
 
         // Initiate Connection
         sftpClient = SftpClient.create(createConnectionParameters())
@@ -227,109 +242,10 @@ class B(val dir: File) : Thread()
                 move(filePath, newFilePath)
                 println("Moved file to sent folder")
             }
-
         }
 
         else {  // Failed Upload
-
             println("Batch Upload Failed")
-
         }
-
-
-    }
-
-    // These environment variables must be defined on your machine
-    private val ENVIRONMENT_VARIABLE_HOST = "KSFTP_HOST"
-    private val ENVIRONMENT_VARIABLE_PORT = "KSFTP_PORT"
-    private val ENVIRONMENT_VARIABLE_USERNAME = "KSFTP_USERNAME"
-    private val ENVIRONMENT_VARIABLE_PASSWORD = "KSFTP_PASSWORD"
-
-    // Remote directory for upload - a folder at the user's root level on SFTP server
-    // Directory will be created if it does not exist
-
-    private var testFiles: Array<File>? = null
-    private var sftpClient: SftpClient? = null
-
-
-    /**
-     * Gets a file from the test resources package, or throws an exception if the test file doesn't exist.
-     * @param relativeFilePath the file path, relative to "src/test/resources"
-     */
-    @Throws(Exception::class)
-    private fun getImagesDirectory(relativeFilePath: String): File {
-        var theRelativeFilePath = relativeFilePath
-        val url = photo_session::class.java.getResource(theRelativeFilePath)
-
-        val testFile = File(url!!.file)
-        TestCase.assertTrue("No test file exists for relative path '$theRelativeFilePath'", testFile.exists())
-        return testFile
-    }
-
-    /**
-     * Ensures that a directory exists for the specified path, and returns the [File],
-     * or `null` if it could not be created.
-
-     * @param directoryPath the directory path to ensure
-     */
-    @Throws(IOException::class)
-    private fun ensureDirectory(directoryPath: String): File {
-        val errorMessage = "Could not create directory for path '$directoryPath'"
-        if (StringUtils.isEmpty(directoryPath)) {
-            throw IOException(errorMessage)
-        }
-
-        val directory = File(directoryPath)
-        if (directory.exists()) {
-            if (!directory.isDirectory) {
-                throw IOException("File '$directory' exists and is not a directory. Unable to create directory.")
-            }
-        } else {
-            if (!directory.mkdirs()) {
-                // Double-check that some other thread or process hasn't made
-                // the directory in the background
-                if (!directory.isDirectory) {
-                    throw IOException("Unable to create directory '$directory'")
-                }
-            }
-        }
-
-        if (!directory.isDirectory) {
-            throw IOException(errorMessage)
-        }
-        return directory
-    }
-
-    /**
-     * Creates new connection parameters.
-     */
-    private fun createConnectionParameters(): SftpConnectionParameters {
-        return SftpConnectionParametersBuilder.newInstance().createConnectionParameters()
-            .withHostFromEnvironmentVariable(ENVIRONMENT_VARIABLE_HOST)
-            .withPortFromEnvironmentVariable(ENVIRONMENT_VARIABLE_PORT)
-            .withUsernameFromEnvironmentVariable(ENVIRONMENT_VARIABLE_USERNAME)
-            .withPasswordFromEnvironmentVariable(ENVIRONMENT_VARIABLE_PASSWORD)
-            .create()
-    }
-
-    @Test
-    @Throws(Exception::class)
-    fun testAllSftpOperations() {
-        executeBatchUpload()
-    }
-
-    @Throws(Exception::class)
-    private fun executeBatchUpload() {
-
-        val remoteFilePaths = java.util.ArrayList<String>()
-        val filePairs = java.util.ArrayList<FilePair>()
-        for (testFile in testFiles!!) {
-            val remoteFilePath = remoteDirectoryForUploads + File.separator + testFile.name
-            filePairs.add(FilePair(testFile.path, remoteFilePath))
-            remoteFilePaths.add(remoteFilePath)
-        }
-
-        TestCase.assertTrue("Files were not uploaded!", sftpClient!!.upload(filePairs, 120*testFiles!!.size))
-        TestCase.assertTrue("Files don't exist on server!", sftpClient!!.checkFiles(remoteFilePaths))
     }
 }
